68aecbc366448cb9bf0c7f4cc2a1c33cfc64eb30
ShadowAJ <ShadowMasterAJ@users.noreply.github.com>
2022-03-18 00:38:23 +0800

    Added Side Menu Drawer UI

--- toilocator/lib/screens/home_map_screen.dart
+++ toilocator/lib/screens/home_map_screen.dart
@@ -1,124 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geocoder/geocoder.dart';
+import 'package:toilocator/palette.dart';
+import 'package:toilocator/widgets/map_stack.dart';
 
-class HomeMapScreen extends StatefulWidget {
+class HomeMapScreen extends StatelessWidget {
   HomeMapScreen({Key? key}) : super(key: key);
 
-  @override
-  State<HomeMapScreen> createState() => _HomeMapScreenState();
-}
-
-class _HomeMapScreenState extends State<HomeMapScreen> {
-  LatLng _initialcameraposition = LatLng(1.3521, 103.8198);
-  late GoogleMapController _controller;
-
-  List<Marker> _markers = [];
-
-  void addMarker() {
-    _markers.add(
-      Marker(
-          markerId: MarkerId(_initialcameraposition.toString()),
-          draggable: false,
-          onTap: () {
-            print(_initialcameraposition.toString());
-          },
-          position: _initialcameraposition),
+  Widget drawerButton(
+    BuildContext ctx,
+    String text,
+    IconData icn,
+    VoidCallback onTap,
+  ) {
+    return InkWell(
+      radius: 200,
+      customBorder: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      onTap: onTap,
+      splashColor: Palette.beige[400],
+      child: Container(
+        width: MediaQuery.of(ctx).size.width,
+        child: Column(children: [
+          SizedBox(
+            height: 15,
+          ),
+          Icon(
+            // splashRadius: 50,
+            // highlightColor: Palette.beige[200],
+            // onPressed: onTap,
+            // icon: Icon(icn),
+            icn,
+            color: Palette.beige[900],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(text),
+          SizedBox(
+            height: 15,
+          ),
+        ]),
+      ),
     );
   }
 
-  void centerToPositionandMark(double lat, double long) {
-    print("Latitude: $lat and Longitude: $long");
-
-    setState(() => _initialcameraposition = LatLng(lat, long));
-    _controller.animateCamera(CameraUpdate.newCameraPosition(
-      CameraPosition(
-        target: _initialcameraposition,
-        zoom: 15,
-      ),
-    ));
-    _markers.clear();
-
-    addMarker();
-  }
-
-  void getCurrentLocation() async {
-    await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-
-    var lat = position.latitude;
-    var long = position.longitude;
-
-    centerToPositionandMark(lat, long);
-  }
-
-  void _onMapCreated(GoogleMapController _cntlr) {
-    _controller = _cntlr;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: Stack(
-            alignment: AlignmentDirectional.topStart,
-            fit: StackFit.loose,
-            children: [
-              GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: CameraPosition(
-                  target: _initialcameraposition,
-                  zoom: 11.0,
-                ),
-                compassEnabled: true,
-                zoomControlsEnabled: true,
-                markers: Set.from(_markers),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  onSubmitted: (value) async {
-                    var addr =
-                        await Geocoder.local.findAddressesFromQuery(value);
-
-                    centerToPositionandMark(addr.first.coordinates.latitude,
-                        addr.first.coordinates.longitude);
-                  },
-                  decoration: InputDecoration(
-                    suffixIcon: Icon(
-                      Icons.search_sharp,
-                      size: 30,
-                      color: Colors.brown.shade600,
-                    ),
-                    filled: true,
-                    fillColor: Colors.amber[100]?.withOpacity(0.9),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Colors.brown.shade100, width: 2),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                    ),
-                    hintText: 'Enter your location',
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 13, vertical: 18),
+        // appBar: AppBar(title: Text('Toilocator')),
+        body: SafeArea(child: MapStack()),
+        drawer: SafeArea(
+          child: Container(
+            width: 100,
+            child: Drawer(
+                backgroundColor: Palette.beige[100],
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Column(
+                    children: [
+                      drawerButton(context, 'Map', Icons.map_outlined, () {}),
+                      drawerButton(context, 'Profile', Icons.person_pin, () {}),
+                      drawerButton(context, 'Settings', Icons.settings, () {}),
+                      drawerButton(
+                          context, 'Favorite\n Toilets', Icons.favorite, () {}),
+                      drawerButton(context, 'Help', Icons.help, () {}),
+                      drawerButton(context, 'Logout', Icons.logout, () {}),
+                      Spacer(),
+                      Container(
+                        child: Center(child: Text('V4.20.69')),
+                        width: double.infinity,
+                        decoration: BoxDecoration(color: Palette.beige[200]),
+                      )
+                    ],
                   ),
-                ),
-              ),
-            ]),
-        floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.amber[300],
-          onPressed: getCurrentLocation,
-          child: Icon(
-            Icons.my_location,
-            color: Colors.black,
+                )),
           ),
         ),
       ),
---
+++ toilocator/lib/widgets/map_stack.dart
@@ -0,0 +1,139 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:toilocator/palette.dart';
+
+class MapStack extends StatefulWidget {
+  const MapStack({Key? key}) : super(key: key);
+
+  @override
+  State<MapStack> createState() => _MapStackState();
+}
+
+class _MapStackState extends State<MapStack> {
+  LatLng _initialcameraposition = LatLng(1.3521, 103.8198);
+  late GoogleMapController _controller;
+
+  List<Marker> _markers = [];
+
+  void addMarker() {
+    _markers.add(
+      Marker(
+          markerId: MarkerId(_initialcameraposition.toString()),
+          draggable: false,
+          onTap: () {
+            print(_initialcameraposition.toString());
+          },
+          position: _initialcameraposition),
+    );
+  }
+
+  void centerToPositionandMark(double lat, double long) {
+    print("Latitude: $lat and Longitude: $long");
+
+    setState(() => _initialcameraposition = LatLng(lat, long));
+    _controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: _initialcameraposition,
+        zoom: 15,
+      ),
+    ));
+    _markers.clear();
+
+    addMarker();
+  }
+
+  void getCurrentLocation() async {
+    await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    var lat = position.latitude;
+    var long = position.longitude;
+
+    centerToPositionandMark(lat, long);
+  }
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+        alignment: AlignmentDirectional.topStart,
+        fit: StackFit.loose,
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _initialcameraposition,
+              zoom: 11.0,
+            ),
+            compassEnabled: true,
+            zoomControlsEnabled: true,
+            markers: Set.from(_markers),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+            child: TextField(
+              onSubmitted: (value) async {
+                var addr = await Geocoder.local.findAddressesFromQuery(value);
+
+                centerToPositionandMark(addr.first.coordinates.latitude,
+                    addr.first.coordinates.longitude);
+              },
+              decoration: InputDecoration(
+                prefixIcon: IconButton(
+                  icon: Icon(
+                    Icons.menu,
+                    size: 20,
+                    color: Palette.beige.shade900,
+                  ),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                ),
+                suffixIcon: Icon(
+                  Icons.search_sharp,
+                  size: 30,
+                  color: Palette.beige.shade800,
+                ),
+                filled: true,
+                fillColor: Palette.beige[100]?.withOpacity(0.9),
+                enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.brown.shade100, width: 2),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    )),
+                focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.brown.shade100, width: 5),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    )),
+                hintText: 'Enter your location',
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 13, vertical: 18),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            left: 10,
+            child: FloatingActionButton(
+              backgroundColor: Palette.beige[200],
+              onPressed: getCurrentLocation,
+              child: Icon(
+                Icons.my_location,
+                color: Palette.beige[900],
+              ),
+            ),
+          ),
+        ]);
+  }
+}
