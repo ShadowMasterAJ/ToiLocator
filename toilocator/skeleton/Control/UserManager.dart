82b42d24a7e7728ec9eed867cc227f89e7f6852e
chen1411 <guningye99@gmail.com>
2022-03-20 22:01:23 +0800

    my commit

--- toilocator/skeleton/Control/UserManager.dart
+++ toilocator/skeleton/Control/UserManager.dart
@@ -16,35 +16,12 @@ class AuthService {
       return null;
     }
   }
+  void checkUser(){}
+  void createUser(){}
+  void updateUser(){} 
+  void deleteUser(){}
+  void connectToFirebase(){}
 
-/// Getter for Firebase user's user ID [uid].
-/*String getUserID(){
-  return _auth.currentUser.uid;
-}
-
-/// Getter for Firebase user's profile image URL [photoURL].
-/*String getUserImageURL(){
-  return _auth.currentUser.photoURL;
-}*/
-
-/// Getter for Firebase user's profile image.
-/**getProfilePic(){
-    if (_auth.currentUser.photoURL != null) {
-      return NetworkImage(_auth.currentUser.photoURL.toString());
-    } else {
-      return Icon(Icons.account_circle, size: 100);
-    }
-}**/
-
-/// Getter for Firebase user's display name [displayName].
-getProfileName(){
-  return _auth.currentUser.displayName.toString();
-}
-
-/// Getter for Firebase user's email [email].
-getEmail(){
-  return _auth.currentUser.email.toString();
-}*/
 
 /// Method to sign out anonymously.
 Future signOut() async {
