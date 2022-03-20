7660e2107da2eb8e97e8ec1797ae035be9c8b8dd
yiqwan <tangyiqwan@MacBook-Air-3.local>
2022-03-20 21:52:02 +0800

    Updated with peilin

--- toilocator/skeleton/Control/ReviewManager.dart
+++ toilocator/skeleton/Control/ReviewManager.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
+
 class ReviewManager {
-
-void updateReview(){
-
+  void updateReview() {}
+  void createReview() {}
+  void deleteReview() {}
+  void searchReview() {}
+  void displayReview() {}
+  float calculateAveRating() {}
 }
-void createReview(){
-
-}
-void deleteReview(){
-
-}
-void searchReview(){
-
-}
-void displayReview(){
-
-}
-float calculateAveRating(){
-
-}
-
-
-}
-
