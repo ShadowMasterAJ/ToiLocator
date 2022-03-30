1179d71ffa10d2ca03cd5851e9a2eeb475f917f3
chen1411 <guningye99@gmail.com>
2022-03-30 17:55:24 +0800

    auth services&provider, incomplete database

---
+++ toilocator/lib/screens/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:toilocator/models/user.dart';
+import 'auth_screen.dart';
+import 'home_map_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+//starting screen switching, not necessary for our app
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
+    
+    // return either the Home or Authenticate widget
+    return AuthScreen();
+    
+  }
+}
--- toilocator/lib/services/auth.dart
+++ toilocator/lib/services/auth.dart
@@ -1,19 +1,61 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:toilocator/models/user.dart';
-
+import 'database.dart';
 class AuthService {
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+ final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  User? _userFromFirebaseUser(FirebaseUser user) {
-    return user!= null ? User(uid: user.uid) : null;
+//the ? makes the return type nullable
+ User? _userFromFirebaseUser(FirebaseUser user) {
+ //return user!= null? User(uid: user.uid) : null;
+ if(user==null) return null;
+ else return User(uid: user.uid);
+ }
+
+
+// auth change user stream
+//when there is a change of users' status, e.g.switching from login to logout,this stream enables the flutter app to show the change by adapting suitable UI?
+//not very sure
+Stream<User?> get user {
+ return _auth.onAuthStateChanged
+ //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+ .map(_userFromFirebaseUser);
+ }
+
+// sign in with email and password
+Future signInWithEmailAndPassword(String email, String password) async{
+  try{
+    AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+    FirebaseUser user = result.user;
+    return _userFromFirebaseUser(user);
+  }catch(e){
+    print(e.toString());
+    return null;
   }
-  // sign in with email and password
+}
 
+// register with email and password
+Future registerWithEmailAndPassword(String email, String password) async{
+  try{
+    AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    FirebaseUser user = result.user;
+    // create a new document for the user with the uid
+    //await DatabaseService(uid: user.uid).updateUserData(true, 'toilet1');
+    return _userFromFirebaseUser(user);
+  }catch(e){
+    print(e.toString());
+    return null;
+  }
+}
 
-
-  // register with email and password
-
-  // sign out
+// sign out
+Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
 
 }
