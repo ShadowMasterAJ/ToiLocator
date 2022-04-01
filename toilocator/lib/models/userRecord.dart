8fa84cfd77d9931245c059d9d135002ab24af8d5
chen1411 <guningye99@gmail.com>
2022-04-01 23:48:51 +0800

    Merge branch 'main' of https://github.com/ShadowMasterAJ/CZ2006_Project

--- toilocator/lib/models/userRecord.dart
+++ toilocator/lib/models/userRecord.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class UserRecord {
   final String gender;
   final int age;
-  final List favToilets;
 
-  UserRecord({required this.gender, required this.age, required this.favToilets});
+  UserRecord({required this.gender, required this.age});
 }
--- toilocator/lib/providers/userList.dart
+++ toilocator/lib/providers/userList.dart
@@ -1,30 +1,30 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:toilocator/models/userRecord.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:toilocator/models/userRecord.dart';
 
 
-//leave it first, may not be used
-//in profile page, nest with a new widget userlist?
-class UserList extends StatefulWidget {
-  @override
-  _UserListState createState() => _UserListState();
-}
+// //leave it first, may not be used
+// //in profile page, nest with a new widget userlist?
+// class UserList extends StatefulWidget {
+//   @override
+//   _UserListState createState() => _UserListState();
+// }
 
-class _UserListState extends State<UserList> {
-  @override
-  Widget build(BuildContext context) {
+// class _UserListState extends State<UserList> {
+//   @override
+//   Widget build(BuildContext context) {
 
-     final userRecord = Provider.of<List<UserRecord>>(context);
-    userRecord.forEach((userRecord) {
-      print(userRecord.gender);
-      print(userRecord.age);
-      print(userRecord.favToilets);
-    });
+//      final userRecord = Provider.of<List<UserRecord>>(context);
+//     userRecord.forEach((userRecord) {
+//       print(userRecord.gender);
+//       print(userRecord.age);
+//       print(userRecord.favToilets);
+//     });
 
 
-    return Container(
+//     return Container(
       
-    );
-  }
-}
+//     );
+//   }
+// }
