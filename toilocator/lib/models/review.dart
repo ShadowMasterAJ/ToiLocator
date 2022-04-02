2f2f5943be87d1261ff73c03b18106e5fb3efa6a
yiqwan <tyq2708@hotmail.com>
2022-04-02 14:32:47 +0800

    Edit toilet and review class, getToiletInfo

---
+++ toilocator/lib/models/review.dart
@@ -0,0 +1,38 @@
+import 'User.dart';
+import 'Toilet.dart';
+
+/// This class stores all the information of a review that is written by a user
+/// 
+class Review {
+  
+  DateTime dateTime = DateTime.now();
+  late String userID;
+  late String toiletID;
+  late int userRating;
+  late String userComment;
+
+  Review(User user, Toilet toilet, int userRating, String userComment) {
+    this.userID = userID;
+    this.toiletID = toiletID;
+    this.userRating = userRating;
+    this.userComment = userComment;
+  }
+
+
+//  /// Get and set method for datetime stored in Review
+//  get getDateTime => this.dateTime;
+ 
+//  /// Get and set method for user stored in Review
+//  get getUserID => this.userID;
+
+//  /// Get and set method for toilet stored in Review
+//  get getToiletID => this.toiletID;
+
+//  /// Get and set method for user rating stored in Review
+//  get getUserRating => this.userRating;
+
+//  /// Get and set method for user comments stored in Review
+//  get getUserComment => this.userComment;
+
+    
+}
