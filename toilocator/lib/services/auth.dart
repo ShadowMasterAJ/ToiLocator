cc275cb6c4f00cfc62e002ed4a3269f58b8bb265
chen1411 <guningye99@gmail.com>
2022-04-02 13:48:44 +0800

    userDatabase

--- toilocator/lib/services/auth.dart
+++ toilocator/lib/services/auth.dart
@@ -41,4 +41,15 @@ Future<void> signOut() async {
 }
 
 
+//auth state change
+// FirebaseAuth.instance
+//   .authStateChanges()
+//   .listen((User? user) {
+//     if (user == null) {
+//       print('User is currently signed out!');
+//     } else {
+//       print('User is signed in!');
+//     }
+//   });
+
 }
