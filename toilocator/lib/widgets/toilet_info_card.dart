4f3e08d47d3200054a231b1a22678ca7fe367a66
catsarenotreal <dengcheng2408@hotmail.com>
2022-04-03 22:57:14 +0800

    UI fixed

--- toilocator/lib/widgets/toilet_info_card.dart
+++ toilocator/lib/widgets/toilet_info_card.dart
@@ -97,37 +97,36 @@ class _toiletInfoCardState extends State<toiletInfoCard> {
   Widget UserReviewInfo(String userID, int userRating, String userComment) {
     //ListView builder probably needed, refer to bottom_panel line 93
     return Container(
-        height: 160,
-        child: Card(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(children: [
-                  Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(userID,
-                          style: Theme.of(context).textTheme.subtitle1)),
-                  // Padding(padding: const EdgeInsets.only(right: 160.0)),
-                  Spacer(),
-                  Row(children: displayStarRating(userRating)),
-                ]),
-                // SizedBox(height: 10),
-                Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Text(
-                      userComment,
-                      maxLines: 5,
-                      textAlign: TextAlign.left,
-                      overflow: TextOverflow.ellipsis,
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 136, 136, 136)),
-                    )),
-                // Padding(padding: const EdgeInsets.only(right: 110.0)),
-                // SizedBox(height: 8),
-                Divider(
-                    color: Color.fromARGB(255, 218, 218, 218), thickness: 1),
-              ]),
-        ));
+      height: 80,
+      // child: Card(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+          Widget>[
+        Row(children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(userID,
+                  style: Theme.of(context).textTheme.subtitle1?.merge(
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 18)))),
+          Padding(padding: const EdgeInsets.only(right: 160.0)),
+          Spacer(),
+          Row(children: displayStarRating(userRating)),
+        ]),
+        // SizedBox(height: 10),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
+            child: Text(
+              userComment,
+              maxLines: 5,
+              textAlign: TextAlign.left,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(color: Color.fromARGB(255, 136, 136, 136)),
+            )),
+        // Padding(padding: const EdgeInsets.only(right: 110.0)),
+        // SizedBox(height: 8),
+        Divider(color: Color.fromARGB(255, 218, 218, 218), thickness: 1),
+      ]),
+    );
+    // );
   }
 
   Route createRoute(int index, List toiletList) {
@@ -162,7 +161,8 @@ class _toiletInfoCardState extends State<toiletInfoCard> {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
           color: Palette.beige[100],
-          child: Row(children: [
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
             Padding(
               padding: const EdgeInsets.all(15.0),
               child: TextButton(
@@ -171,6 +171,37 @@ class _toiletInfoCardState extends State<toiletInfoCard> {
                   "Directions",
                 ),
                 onPressed: null,
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(15)),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Palette.beige[300] as Color),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 255, 255, 255)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                      ),
+                    )),
+              ),
+            ),
+            // Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextButton(
+                onPressed: () => Navigator.of(context).push(createRoute(
+                    widget.index,
+                    widget.toiletList)), // ONLY IF USER IS AUTHENTICATED
+                child: Text(
+                  "Write Review...",
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
                 style: ButtonStyle(
                   padding:
                       MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
@@ -187,36 +218,32 @@ class _toiletInfoCardState extends State<toiletInfoCard> {
                 ),
               ),
             ),
-            SizedBox(width: 58),
-            Text(widget.indices[widget.index].toString() + "m",
-                style: Theme.of(context).textTheme.headline5?.merge(TextStyle(
-                    color:
-                        Color.fromARGB(255, 87, 62, 25))) // change colour too
-                ),
-            // SizedBox(width: 65),
-            Spacer(),
+            // Spacer(),
             Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: Row(children: [
                   TextButton(
-                      child: Text(
-                        "Back",
+                    child: Text(
+                      "Back",
+                    ),
+                    onPressed: () => Navigator.of(context).pop(),
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.all(15)),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Palette.beige[300] as Color),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 255, 255, 255)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
                       ),
-                      onPressed: () => Navigator.of(context).pop(),
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.all(15)),
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                              Palette.beige[300] as Color),
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Color.fromARGB(255, 255, 255, 255)),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18.0),
-                                      side: BorderSide(
-                                          color: Color.fromARGB(
-                                              255, 255, 255, 255))))))
+                    ),
+                  ),
                 ])),
           ])),
       body: SingleChildScrollView(
@@ -226,26 +253,52 @@ class _toiletInfoCardState extends State<toiletInfoCard> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
-                height: 12.0,
+                height: 8.0,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 30,
-                    height: 5,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.all(Radius.circular(12.0))),
-                  ),
-                ],
-              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: <Widget>[
+              //     Container(
+              //       width: 30,
+              //       height: 5,
+              //       decoration: BoxDecoration(
+              //           color: Colors.grey[300],
+              //           borderRadius: BorderRadius.all(Radius.circular(12.0))),
+              //     ),
+              //   ],
+              // ),
               Divider(
                 thickness: 1.5,
                 indent: 10,
                 endIndent: 10,
               ),
-              SizedBox(height: 20),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextButton(
+                    child: Text(
+                      widget.indices[widget.index].toString() + "m",
+                    ),
+                    onPressed: null,
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.all(12)),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 255, 255, 255)),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(255, 179, 152, 112)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(
+                              color: Color.fromARGB(255, 179, 152, 112)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 3),
               Container(
                 width: MediaQuery.of(context).size.width * 1,
                 child: Text(
@@ -344,76 +397,48 @@ class _toiletInfoCardState extends State<toiletInfoCard> {
                 ),
               ),
               Container(
-                  height: 180,
-                  child: FutureBuilder(
-                    future: createImageList(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<dynamic> snapshot) {
-                      return SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: imageList,
-                        ),
-                      );
-                    },
-                  )),
+                height: 180,
+                child: FutureBuilder(
+                  future: createImageList(),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                    return SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: imageList,
+                      ),
+                    );
+                  },
+                ),
+              ),
               Divider(
-                  color: Color.fromARGB(255, 114, 114, 114),
+                  color: Color.fromARGB(255, 142, 142, 142),
                   thickness: 4,
                   indent: 30,
                   endIndent: 30),
               SizedBox(height: 12),
               Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Row(
+                padding: const EdgeInsets.only(left: 00),
+                child: Column(
                   children: [
                     Text("User Reviews",
                         style: Theme.of(context).textTheme.headline6),
-                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                      SizedBox(width: 115),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: TextButton(
-                          onPressed: () => Navigator.of(context).push(createRoute(
-                              widget.index,
-                              widget
-                                  .toiletList)), // ONLY IF USER IS AUTHENTICATED
-                          child: Text(
-                            "Write Review...",
-                            style: TextStyle(
-                              fontSize: 13,
-                              color: Color.fromARGB(255, 185, 185, 185),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ]),
-                    Divider(
-                        color: Color.fromARGB(255, 114, 114, 114),
-                        thickness: 4,
-                        indent: 30,
-                        endIndent: 30),
-                    SizedBox(height: 12),
-                    Padding(
-                        padding: const EdgeInsets.only(left: 20),
-                        child: Text("User Reviews",
-                            style: Theme.of(context).textTheme.headline6)),
-                    SizedBox(height: 6),
+                    SizedBox(height: 8),
                     Divider(
                         color: Color.fromARGB(255, 218, 218, 218),
                         thickness: 2),
                     SizedBox(height: 6),
                     Container(
-                      height: 180,
+                      height: 300,
                       child: FutureBuilder(
                         future: createReviewList(),
                         builder: (BuildContext context,
                             AsyncSnapshot<dynamic> snapshot) {
                           return SingleChildScrollView(
                             scrollDirection: Axis.vertical,
-                            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+                            padding: EdgeInsets.fromLTRB(8.0, 0.0, 10.0, 0.0),
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: reviewList,
