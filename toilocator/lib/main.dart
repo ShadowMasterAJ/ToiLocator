b05a79d38e26cd9bfcf815b1194caff1dc58fe8c
yiqwan <tyq2708@hotmail.com>
2022-04-04 19:59:25 +0800

    Removed default font

--- toilocator/lib/main.dart
+++ toilocator/lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'ToiLocator',
         theme: ThemeData(
-          fontFamily: 'Avenir',
+          // fontFamily: 'Avenir',
           backgroundColor: Palette.beige[50],
           textTheme: const TextTheme(
             headline1: TextStyle(
