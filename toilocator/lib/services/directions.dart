803b1edaf4d0f4590d1343104f74ccfd66bfb8f8
ShadowAJ <ShadowMasterAJ@users.noreply.github.com>
2022-04-09 16:12:12 +0800

    Ignore

--- toilocator/lib/services/directions.dart
+++ toilocator/lib/services/directions.dart
@@ -1,31 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_directions_api/google_directions_api.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:url_launcher/url_launcher.dart';
-
-// class Direction {
-//   final LatLng source;
-//   final LatLng dest;
-
-//   var destination;
-
-//   var origin;
-//   var key;
-//   var YOUR_API_KEY = 'AIzaSyBD6svvL_5JkLGtN9NG3V1KMx28IJ0Jiog';
-
-//   Direction({required this.source, required this.dest});
-
-//   Future<http.Response> getDirections(source, dest) async {
-//     final url = Uri.parse(
-//         "https://maps.googleapis.com/maps/api/directions/json?destination=$source&origin=$dest&key=$YOUR_API_KEY&mode=walking");
-
-//     final response = await http.get(url);
-//     Map<String, List> dirData = {};
-//     final Set<Polyline> polyline = {};
-//   }
-// }
-
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
